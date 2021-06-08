bca903bdb3fd7b7b303a23d60dc2ec58b6f38f26
Dewey <hammersdrichard@gmail.com>
2021-06-08 11:23:48 -0700

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,17 @@
-// Code your solutions in this file
+function writeCards(array, event) {
+  const thankYous = []
+
+  for (let i = 0; i < array.length; i++) {
+    thankYous.push(`Thank you, ${array[i]}, for the wonderful ${event} gift!`)
+  }
+
+  return thankYous
+}
+
+function countDown(count) {
+  let i = -1
+  while (i < count) {
+    console.log(count)
+    count--
+  }
+}
